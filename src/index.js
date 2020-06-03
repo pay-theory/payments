c958ab877b9585c6c1b67c1b86cbd7dd2c1cf98e
EC2 Default User <ec2-user@ip-172-31-77-21.ec2.internal>
2020-06-03 15:28:33 +0000

    updated tags

--- src/index.js
+++ src/index.js
@@ -1,46 +1,56 @@
 import React, { useState, useEffect, useContext } from "react"
 import tags from '@paytheory/tags'
 
-export const PayTheoryContext = React.createContext()
-export const usePayTheory = () => useContext(PayTheoryContext)
-export const CreditCardInput = () => (<div id="paytheory-credit-card" />)
-export const CreditCardProvider = (props) => {
-    const [loading, setLoading] = useState(true)
-    const [complete, setComplete] = useState(false)
-    const [valid, setValid] = useState(false)
-    const [error, setError] = useState(undefined)
-    const [payTheory, setPayTheory] = useState(undefined)
+export const PayTheoryContext = React.createContext();
+export const usePayTheory = () => useContext(PayTheoryContext);
+export const CreditCardInput = () => <div id="paytheory-credit-card" />;
+export const CreditCardProvider = props => {
+    const [loading, setLoading] = useState(true);
+    const [complete, setComplete] = useState(false);
+    const [valid, setValid] = useState(false);
+    const [error, setError] = useState(undefined);
+    const [payTheory, setPayTheory] = useState(undefined);
 
     useEffect(() => {
         const initPayTheory = async() => {
-            const payTheoryFromHook = await tags.createCreditCard(props.apiKey, props.client, props.amount, props.styles)
-            setPayTheory(payTheoryFromHook)
-            setLoading(false)
+            const payTheoryFromHook = await tags.createCreditCard(
+                props.apiKey,
+                props.client,
+                props.amount,
+                props.styles
+            );
+            setPayTheory(payTheoryFromHook);
+            setLoading(false);
         };
-        initPayTheory()
-    }, [])
+        if (props.apiKey && props.client && props.amount && props.styles) {
+            initPayTheory();
+        }
+    }, [props.apiKey, props.client, props.amount, props.styles]);
 
     useEffect(() => {
         if (payTheory) {
-            payTheory.readyObserver(ready => setLoading(!ready))
-            payTheory.transactedObserver(acted => setComplete(acted))
-            payTheory.validObserver(validation => setValid(validation))
-            payTheory.errorObserver(errored => setError(errored))
+            payTheory.readyObserver(ready => setLoading(!ready));
+            payTheory.transactedObserver(acted => setComplete(acted));
+            payTheory.validObserver(validation => setValid(validation));
+            payTheory.errorObserver(errored => setError(errored));
         }
-    }, [payTheory])
+    }, [payTheory]);
 
     return (
         <PayTheoryContext.Provider
-          value={{
-            loading,
-            complete,
-            valid,
-            error,
-            mount: payTheory ? payTheory.mount : () => console.log('pay theory is not ready to mount payments'),
-            initTransaction: buyerOptions => tags.initTransaction(buyerOptions ? buyerOptions : {})
-          }}
-        >
-            {props.children}
-        </PayTheoryContext.Provider>
+      value={{
+        loading,
+        complete,
+        valid,
+        error,
+        mount: payTheory
+          ? payTheory.mount
+          : () => console.log("pay theory is not ready to mount payments"),
+        initTransaction: buyerOptions =>
+          tags.initTransaction(buyerOptions ? buyerOptions : {})
+      }}
+    >
+      {props.children}
+    </PayTheoryContext.Provider>
     );
-}
+};
